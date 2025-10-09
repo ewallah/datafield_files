fa6b1516f2c004f9ba28be64e10d831e39cf88b7
Renaat Debleu <rdebleu@eWallah.net>
2025-10-09 14:23:48 +0000

    Version update

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'datafield_files';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = 'v5.0.1';
-$plugin->version = 2025082500;
-$plugin->requires = 2024100700;
-$plugin->supported = [405, 500];
+$plugin->release = 'v5.1.1';
+$plugin->version = 2025100900;
+$plugin->requires = 2025041400;
+$plugin->supported = [500, 501];
